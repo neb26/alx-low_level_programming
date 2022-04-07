81faafd688e7880881cd8029c3dc7a79024d4ded
root <nebamagna@gmail.com>
2022-04-07 05:59:45 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+table File  12 lines (11 sloc)  156 Bytes
+   
+#include "main.h"
+/**
+ * mul -multiplies two integers
+ * @a: print int
+ * @b: print int
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
